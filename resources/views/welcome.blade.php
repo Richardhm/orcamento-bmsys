a09cf01cdbe54104af5c056911f42cd427650c4b
Richard <richardjonhshm@gmail.com>
2025-01-21 09:56:20 -0300

    Versão Mobile correcoes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -126,8 +126,8 @@
     </div>
 </section>
 
-<section id="pricing" class="h-screen flex items-center justify-center bg-blue-500 text-white text-center">
-    <div>
+<section id="pricing" class="h-screen flex items-center justify-center bg-blue-500 text-white text-center py-10 mt-1">
+    <div class="container mx-auto px-4">
         <h2 class="text-3xl lg:text-5xl font-bold">Planos Acessíveis</h2>
         <div class="mt-8 flex flex-col lg:flex-row gap-6 justify-center">
             <div class="bg-white text-blue-500 rounded-lg shadow-lg p-6 w-72">
