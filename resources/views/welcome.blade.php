25ca2a8fa52ba85155fc69b7f36d3b1845e0d36a
Richard <richardjonhshm@gmail.com>
2025-03-19 17:39:14 -0300

    Colocar Welcome responsive #5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,6 +124,19 @@
             .text-4xl { font-size: 1.8rem; }
             .text-6xl { font-size: 2.5rem; }
             .text-7xl { font-size: 3rem; }
+
+
+            #imagem-dobra {
+                padding-top: 6rem; /* 96px - ajuste conforme necessário */
+            }
+
+            /* Opcional: ajustar altura mínima se necessário */
+            #imagem-dobra {
+                min-height: calc(100vh - 6rem);
+            }
+
+
+
         }
 
         @media (max-width: 480px) {
@@ -197,7 +210,6 @@
 
 
 <section id="imagem-dobra" class="relative min-h-screen flex items-center justify-center pt-24 md:pt-16 pb-20 overflow-hidden">
-
     <!-- Background pattern -->
     <div class="absolute inset-0 opacity-10" style="background-image: url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23ffffff\" fill-opacity=\"0.05\"%3E%3Cpath d=\"M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E');"></div>
 
