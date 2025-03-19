1658278131a78d8a9fe157955f9773a6c54039b3
Richard <richardjonhshm@gmail.com>
2025-03-19 17:31:23 -0300

    Colocar Welcome responsive #5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,9 +59,10 @@
         @media (max-width: 768px) {
             /* Ajustes para navbar */
             nav .flex.justify-between {
-                flex-direction: column;
-                height: auto;
-                padding: 10px 0;
+                /*flex-direction: column;*/
+                /*height: auto;*/
+                /*padding: 10px 0;*/
+
             }
 
             .absolute.left-1\/2 {
@@ -139,6 +140,7 @@
 
             #imagem-dobra .scale-75 {
                 scale: 0.6;
+
             }
 
 
@@ -194,7 +196,7 @@
 
 
 
-<section id="imagem-dobra" class="relative min-h-screen flex items-center justify-center pt-16 pb-20 overflow-hidden">
+<section id="imagem-dobra" class="relative min-h-screen flex items-center justify-center pt-24 md:pt-16 pb-20 overflow-hidden">
 
     <!-- Background pattern -->
     <div class="absolute inset-0 opacity-10" style="background-image: url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23ffffff\" fill-opacity=\"0.05\"%3E%3Cpath d=\"M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E');"></div>
