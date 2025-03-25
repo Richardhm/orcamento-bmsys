b5dda902bbf0866c6ca304cc5e785982b9999cf7
Richard <richardjonhshm@gmail.com>
2025-03-25 14:51:40 -0300

    Tradução da pagina de perfil #5

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -79,7 +79,7 @@
             </div>
 
             <div class="flex items-center gap-4">
-                <x-primary-button>{{ __('Save') }}</x-primary-button>
+                <x-primary-button>{{ __('Salvar') }}</x-primary-button>
 
                 @if (session('status') === 'profile-updated')
                     <p
