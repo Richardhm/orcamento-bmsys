387a769eb26baef89af3fe8367afc59799267f77
Richard <richardjonhshm@gmail.com>
2025-03-25 09:54:08 -0300

    Bug Login Csrf-Token #4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\PreventSimultaneousLogins;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\MobileSessionFix;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(MobileSessionFix::class);
         $middleware->alias([
             'prevent-simultaneous-logins' => PreventSimultaneousLogins::class,
         ]);
