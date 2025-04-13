01c1aefd0fd7955fafd8fcb7c4011c4862fd55ce
Richard <richardjonhshm@gmail.com>
2025-04-13 17:44:45 -0300

    Configurando o verify-email #2

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="w-[70%] mx-auto bg-[rgba(254,254,254,0.18)] backdrop-blur-[15px] p-5 rounded">
-        <div class="mb-4 text-sm text-white dark:text-white">
+        <div class="mb-4 text-sm text-white dark:text-white text-center">
             {{ __('Obrigado por se cadastrar! Antes de começar, você pode verificar seu endereço de e-mail clicando no link que acabamos de enviar para você? Se você não recebeu o e-mail, teremos o prazer de enviar outro.') }}
         </div>
 
@@ -24,7 +24,7 @@
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
 
-                <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                <button type="submit" class="underline text-sm text-white dark:text-white hover:text-white dark:hover:text-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                     {{ __('Sair') }}
                 </button>
             </form>
